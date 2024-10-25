a83d560fd9074e9b33b65531e836f20f1a18985b
risqanalhabib <risqanalhabib60@gmail.com>
2024-10-25 12:58:11 +0700

    hapus

--- kabar_berita.php
+++ kabar_berita.php
@@ -15,34 +15,34 @@
         <h2 class="text-center text-white mt-3">BERITA</h2>
         <a href="tambah_berita" class="btn btn-success">Tambah Berita</a>
         <div class="row">
-            <div class="col-lg-4">
+            <div class="col-lg-4 col-md-6">
                 <div class="card">
                     <img src="../foto/suzuki1.png" class="card-img-top">
                     <div class="card-body">
-                        <h5 class="card-title">Suzuki Luncurkan Skutik Retro Baru</h5>
-                        <a href="berita1.php" class="btn btn-primary">Detail</a>
+                        <h5 class="card-title">Suzuki Luncurkan Skutik Retro Baru resmi meluncurkan Suzuki US125T</h5>
+                        <a href="berita1.php" class="btn btn-primary btn-detail">Detail</a>
                     </div>
                 </div>
             </div>
 
            
-            <div class="col-lg-4">
+            <div class="col-lg-4 col-md-6">
                 <div class="card">
                     <img src="../foto/m.tedy1.jpg" class="card-img-top">
                     <div class="card-body">
                         <h5 class="card-title">Rincian Gaji Mayor Teddy Sebagai Sekretaris Kabinet & TNI Aktif</h5>
-                        <a href="berita2.php" class="btn btn-primary">Detail</a>
+                        <a href="berita2.php" class="btn btn-primary btn-detail">Detail</a>
                     </div>
                 </div>
             </div>
 
         
-            <div class="col-lg-4">
+            <div class="col-lg-4 col-md-6">
                 <div class="card">
                     <img src="../foto/wok1.jpg"  class="card-img-top">
                     <div class="card-body">
                         <h5 class="card-title">Prabowo Minta Kapolri-Jaksa Agung Fokus Perangi Korupsi</h5>
-                        <a href="berita3.php" class="btn btn-primary">Detail</a>
+                        <a href="berita3.php" class="btn btn-primary btn-detail">Detail</a>
                     </div>
                 </div>
             </div>
---
+++ tambah_berita.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah berita</title>
+
+    <!-- link css -->
+    <link rel="stylesheet" href="../css_berita/tambah_berita.css">
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+
+</head>
+<body>
+<div class="card-body">
+    <a href="kabar_berita.php" class="card-link">Back</a>
+    <h3 class="card-title">Tambah berita</h3>
+  <label for="exampleInputberita">judul Berita</label>
+  <input type="email" class="form-control" id="exampleInputberita">
+
+  
+ 
+
+
+  <div class="card-body">
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+</div>
+</body>
+</html>
