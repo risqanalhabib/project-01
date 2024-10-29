483f92eb2cdce121dff4b8abc31b9038a071ad20
khairatulnisa <khairatulnisa616@gmail.com>
2024-10-29 07:40:14 +0000

    mengubah

--- index.php
+++ index.php
@@ -1,66 +1,79 @@
 <?php
+// index.php
 ?>
-<!doctype html>
-<html lang="en">
-  <head>
-         <link rel="icon" href="foto/logo1.jpg" sizes="64x64" type="image/jpg"> 
+
+<!DOCTYPE html>
+<html lang="id">
+
+
+
+
+<head>
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" sizes="32x32" href="foto/logo1.jpg">
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-    <title>Project PKL</title>
-
+    <title>Project Bersama_1</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-  </head>
-  <body class="p-3 m-0 border-0 bd-example m-0 border-0">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css-1/index.css">
 
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-          <img src="foto/logo1.jpg" alt="logo" width="60" height="60">
+    <!-- Berita CSS -->
+    <link rel="stylesheet" href="css_berita/kabar_berita.css">
 
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="pgs/about.php">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="pgs/kabar_berita.php">Berita</a>
-            </li>
-            
-            <li class="nav-item">
-            <div class="bg-body-tertiary">
-          <div class="container mt-5 pt-5">
-          <div class="card p-4">
+    <!-- About CSS -->
+    <link rel="stylesheet" href="css-1/about.css">
+</head>
 
-          <?php include("pgs/header.php"); ?>
 
-          </div>
-          </div>
+<body>
+    <!-- Navigasi Menu -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="foto/logo1.jpg" alt="Logo" width="70" height="50">
+            </a>
 
-          <hr class="container mt-5 mb-2">
-
-          <?php include("pgs/about.php"); ?> 
-          <hr class="container mt-5 mb-2">
-          <?php include("pgs/kabar_berita.php"); ?>
-    </div>
-            </li>
-          </ul>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active m-2" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link m-2" href="#about-section">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link m-2" href="#berita-section">Berita</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="btn m-2 bg-black fw-medium text-light" href="pages/login.html" style="width: auto; padding-left: 30px; padding-right: 30px;">Login</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-        </div>
-      </div>
     </nav>
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
-  </body>
+    <?php Include("pgs/header.php"); ?>
+
+        <hr class="container mt-5 mb-2">
+        
+
+        <!-- Include About dan Berita -->
+        <?php include("pgs/about.php"); ?>
+        <hr class="container mt-5 mb-2">
+        <?php include("pgs/kabar_berita.php"); ?>
+    </div>
+    <!-- Bootstrap 5.3.3 JS Bundle -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+DmZhu9Kq5zY1N4NjZkQ+gxGjkM95" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+</body>
+
 </html>
