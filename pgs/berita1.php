992880330cd58beb75a00ba87843b6f8a9baac3d
khairatulnisa <khairatulnisa616@gmail.com>
2024-10-30 13:03:43 +0000

    rubah semua

--- pgs/berita1.php
+++ pgs/berita1.php
@@ -3,38 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-     <!-- link bootstrap -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    
+    <!-- Link Bootstrap -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 
-     <!-- link css -->
-    <link rel="stylesheet" href="css_berita/berita1.css">
-    <title>Suzuki Skutik Retro baru</title>
+    <!-- Link CSS -->
+    <link rel="stylesheet" href="../css_berita/berita1.css">
+    <title>Suzuki Skutik Retro Baru</title>
 </head>
 <body>
-    <a href="../index.php" class="btn btn-primary">kembali</a>
-    <div class="container-fluid"> 
-    <div class="container">
-    <div class="row row-cols-1 row-cols-md-2 g-4">
-        <div class="col">
-          <div class="card">
-            <img src="../foto/suzuki1.png" class="card-img-top" width="400px">
-            <div class="card-body">
-              <h5 class="card-title">Suzuki Luncurkan Skutik Retro Baru</h5>
-              <p class="card-text">Produsen roda dua asal Jepang, Suzuki, resmi meluncurkan Suzuki US125T untuk konsumen di China.
-                Skuter matik tersebut punya tampang retro seperti motor-motor besutan Italia. Lantas, apa saja keunggulan yang ditawarkan?
-                Suzuki US125T punya bobot kosong 110 kg dengan kapasitas bahan bakar 5,9 liter. Kemudian bannya berukuran 12 inch di depan dan 10 inch di belakang.
-                Remnya menggunakan cakram tunggal yang terpasang di roda depan.
-              </p>
+    <a href="../index.php" class="btn btn-primary">Kembali</a>
+    <div class="d-flex justify-content-center align-items-center vh-100">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <img src="../foto/suzuki1.png" class="card-img-top img-fluid" alt="Suzuki US125T">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Suzuki Luncurkan Skutik Retro Baru</h5>
+                            <p class="card-text">
+                                Produsen roda dua asal Jepang, Suzuki, resmi meluncurkan Suzuki US125T untuk konsumen di China.
+                                Skuter matik tersebut punya tampang retro seperti motor-motor besutan Italia. Lantas, apa saja keunggulan yang ditawarkan?
+                                Suzuki US125T punya bobot kosong 110 kg dengan kapasitas bahan bakar 5,9 liter. Kemudian bannya berukuran 12 inch di depan dan 10 inch di belakang.
+                                Remnya menggunakan cakram tunggal yang terpasang di roda depan.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-        </div>
+    </div>
 
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
+    <!-- Script Bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
--- pgs/habib.php
+++ pgs/habib.php
@@ -1,61 +1,56 @@
-<?php
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-          <meta charset="UTF-8">
-          <meta http-equiv="X-UA-Compatible" content="IE=edge">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Detail</title>
-          <link rel="stylesheet" href="css/bootstrap.min.css">
-          <link rel="stylesheet" href="../css-1/bib.css">
-
-          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css-1/bib.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
-<div class="card mb-3">
-  <div class="row">
-    <div class="col-md-4">
-    <img src="../foto/hb.jpg" class="card-img-top" alt="habib">
+<body class="body-bib">
+    <div class="card mb-3">
+        <div class="row g-5 align-items-center">
+            <div class="col-md-4">
+                <img src="../foto/hb.jpg" class="card-img-top img-fluid img-habib" alt="habib">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title1">Risqan Al Habib</h5>
+                    <p class="card-text"><small>Rekayasa Perangkat Lunak</small></p>
+                    <div class="table-responsive content">
+                        <table class="table">
+                            <tr>
+                                <td>Email:</td>
+                                <td>risqanalhabib60@gmail.com</td>
+                            </tr>
+                            <tr>
+                                <td>No. Hp:</td>
+                                <td>089525175378</td>
+                            </tr>
+                            <tr>
+                                <td>Tempat Tinggal:</td>
+                                <td>Kp Dalam Gadur</td>
+                            </tr>
+                            <tr>
+                                <td>Tempat Tanggal Lahir:</td>
+                                <td>Pauh Kambar, 23 Maret 2007</td>
+                            </tr>
+                            <tr>
+                                <td>Bio Profil:</td>
+                                <td>Hai, perkenalkan nama saya Risqan Al Habib, saya biasa di panggil habib umur saya 17 tahun, saya berasal dari sekolah SMK N 1 SINTOGA, salam kenal.</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <a href="../index.php" class="btn btn-success mt-3">Back</a>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Risqan Al Habib</h5>
-        <p class="card-text"><small>Rekayasa Perangkat Lunak</small></p>
-<div class="content">
-		<table>
-			<tr>
-				<td> Email :</td>
-				<td> risqanalhabib60@gmail.com </td>
-			</tr>
 
-      <tr>
-      <td> No.Hp : </td>
-				<td> 089525175378 </td>
-      </tr>
-
-      <tr>
-      <td > Tempat Tinggal : </td>
-				<td> Kp Dalam Gadur</td>
-      </tr>
-
-      <tr>
-      <td > Tempat tanggal lahir :</td>
-				<td> Pauh Kambar, 23 Maret 2007 </td>
-      </tr>
-
-      <tr>
-      <td > Bio Profil :</td>
-				<td>  Hai, perkenalkan nama saya Risqan Al Habib, saya biasa di panggil habib umur saya 17 tahun, saya berasal dari sekolah SMK N 1 SINTOGA, salam kenal.</td>
-      </tr>
-</table><br>
-<a href="../index.php" class="btn btn-success">back</a>
-
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
-
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
